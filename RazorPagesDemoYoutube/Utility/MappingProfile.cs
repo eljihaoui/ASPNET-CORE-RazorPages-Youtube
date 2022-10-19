b3580f81d826f20f94a5d3c053108f93a45d4503
MOHAMED EL JIHAOUI <eljihaoui@gmail.com>
2022-10-19 15:08:38 +0100

    Add project files.

---
+++ RazorPagesDemoYoutube/Utility/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RazorPagesDemoYoutube.Models;
+using RazorPagesDemoYoutube.ViewModels;
+
+namespace RazorPagesDemoYoutube.Utility
+{
+    public class MappingProfile :Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CategoryViewModel>().ReverseMap();
+            CreateMap<AppUser, UserInputVeiwModel>().ReverseMap();
+        }
+    }
+}
