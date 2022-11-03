6279e79bc5a328c5756d3542d8e9cd842c261568
MOHAMED EL JIHAOUI <eljihaoui@gmail.com>
2022-11-03 02:28:16 +0100

    List of Courses Get Handler

--- RazorPagesDemoYoutube/Pages/Admin/Courses/AddOrEdit.cshtml.cs
+++ RazorPagesDemoYoutube/Pages/Admin/Courses/AddOrEdit.cshtml.cs
@@ -2,9 +2,11 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using NToastNotify;
 using RazorPagesDemoYoutube.Data;
 using RazorPagesDemoYoutube.Models;
+using RazorPagesDemoYoutube.Utility;
 using RazorPagesDemoYoutube.ViewModels;
 
 namespace RazorPagesDemoYoutube.Pages.Admin.Courses
@@ -19,12 +21,17 @@ namespace RazorPagesDemoYoutube.Pages.Admin.Courses
         private readonly CourseDbContext _db;
         private readonly IMapper _mapper;
         private readonly IToastNotification _notify;
+        private readonly IWebHostEnvironment _host;
 
-        public AddOrEditModel(CourseDbContext db, IMapper mapper, IToastNotification notify)
+        public AddOrEditModel(CourseDbContext db, IMapper mapper,
+            IToastNotification notify,
+            IWebHostEnvironment host
+            )
         {
             this._db = db;
             this._mapper = mapper;
             this._notify = notify;
+            this._host = host;
         }
         public void OnGet()
         {
@@ -44,9 +51,16 @@ namespace RazorPagesDemoYoutube.Pages.Admin.Courses
 
         public async Task<IActionResult> OnPost()
         {
+            string webroot = _host.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+            string ImageFolder = @"Images\Courses";
+            string UploadFolder = Path.Combine(webroot, ImageFolder);
+
             if (ModelState.IsValid)
             {
                 Course course = _mapper.Map<Course>(CourseViewModel);
+                string fileNewName = await FileManager.CopyFile(files[0], UploadFolder);
+                course.ImageUrl = Path.Combine(ImageFolder, fileNewName);
                 await _db.Course.AddAsync(course);
                 bool res = await _db.SaveChangesAsync() > 0;
                 if (res)
